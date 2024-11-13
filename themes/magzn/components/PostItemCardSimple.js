76d341fc7d28ed2fb40c4689f09ae8040ebd62b6
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2024-11-13 23:47:23 +0800

    Update PostItemCardSimple.js

--- themes/magzn/components/PostItemCardSimple.js
+++ themes/magzn/components/PostItemCardSimple.js
@@ -19,29 +19,23 @@ const PostItemCardSimple = ({ post, showSummary }) => {
     <div
       key={post.id}
       className='lg:mb-6 max-w-screen-3xl border-t border-gray-300 mr-8 py-2 gap-y-3 flex flex-col dark:border-gray-800 '>
-      <div className='flex mr-2 items-center'>
-      {siteConfig('MAGZINE_POST_LIST_COVER') && (
-          <Link
-            href={post?.href}
-            passHref
-            className={
-              'cursor-pointer hover:underline leading-tight text-gray-700 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-400'
-            }>
-            <div className='w-full h-40 aspect-video overflow-hidden mb-2'>
-              <LazyImage
-                alt={post?.title}
-                src={cover}
-                style={cover ? {} : { height: '0px' }}
-                className='w-full h-40 aspect-video object-cover'
-              />
-            </div>
-          </Link>
-        )}
-        {/* 此处代码注释并在前面同级增加以下代码： */}
-        {/* {siteConfig('MAGZINE_POST_LIST_CATEGORY') && (
-          <CategoryItem category={post.category} />
-        )} */}
-      </div>
+    {siteConfig('MAGZINE_POST_LIST_COVER') && (
+              <Link
+                href={post?.href}
+                passHref
+                className={
+                  'cursor-pointer hover:underline leading-tight text-gray-700 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-400'
+                }>
+                <div className='w-full h-40 aspect-video overflow-hidden mb-2'>
+                  <LazyImage
+                    alt={post?.title}
+                    src={cover}
+                    style={cover ? {} : { height: '0px' }}
+                    className='w-full h-40 aspect-video object-cover'
+                  />
+                </div>
+              </Link>
+            )}
 
       {/* 文章标题 */}
       <Link
